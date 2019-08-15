71a6c6f5965efd9f1f683fe5dc7ad5d6104babb2
Boyce_Scout <49099741+ChrisBoyce886@users.noreply.github.com>
2019-08-15 18:36:33 -0400

    completed main game logic, finished displaying scores to results page, updated all HTML tag ID's to match logic, styled timer. Need to figure out one bug left in radio selection

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,7 +1,12 @@
-
-
+$(document).ready(function() {
+//Global variables
 var clock
+var selected
+var correct = 0
+var wrong = 0
 
+
+//Trivia question answers
 var answers = {
     q1: "13.8 billion years old",
     q2: "Elliptical galaxies",
@@ -15,23 +20,13 @@ var answers = {
     q10: "True"
 };
 
-var choices = {
-    q1: [""],
-    q2: [""],
-    q3: [""],
-    q4: [""],
-    q5: [""],
-    q6: [""],
-    q7: [""],
-    q8: [""],
-    q9: [""],
-    q10: [""]
-};
 
+//On page start, show only game name and start button, hide everything else
 $("#index2").hide();
 $("#index3").hide();
 $("#submitBtn").hide();
 
+//Function to display game results page, hide everything else
 function display(){
     $("#index2").hide();
     $("#index3").show();
@@ -39,33 +34,192 @@ function display(){
     $(".clock").hide(); 
 };
 
+//Click function to start timer and display trivia game after start button is pressed, hide everything else
 $("#startBtn").on("click", function () {
     $("#start-page").hide();
     $("#index2").show();
     $("#submitBtn").show();
-    var clock = new FlipClock($('.clock'), 90, {
+    //Function to display timer countdown, set for 1min 30sec.
+    var clock = new FlipClock($(".clock"), 90, {
         countdown: true,
         clockFace: 'MinuteCounter',
+        //when timer hits 00:00, automatically end game and call 'display' function to display game results
         callbacks: {
             stop: function() {
+                //auto scroll to top of page to counter a bug in falling stars background
                 $("html, body").animate({ scrollTop: 0 }, "fast");
                 setTimeout(function(){
-                display() 
-                }),3000          
-            }
+                display();
+                }),3000        }
         }
     });
 });    
 
+//Click function to display game results page if submit button is pressed before time runs out
 $("#submitBtn").on("click", function () {
+    //auto scroll to top of page to counter a bug in falling stars background
     $("html, body").animate({ scrollTop: 0 }, "fast");
     setTimeout(function(){
-    display()
+    display();
     }),3000  
+});
+
+//Game logic
+//Grab value of selected answer 
+
+// var name = []
+//   $('.card-body input').on('change', function() {
+// for (i = 0; i <= 10; i++){
+//     name[i] = $('input[name=inlineRadioOptions'+i+']:checked', '.card-body').val();
+//     console.log(name[0])
+
+//  if (name[i] === answers.q1){
+//         correct++
+//         console.log("Correct: " + correct)
+//     }   
+// }
+
+
+
+
+$('.card-body1 input').on('change', function() {
+    answer1 = $('input[name=inlineRadioOptions1]:checked', '.card-body1').val();
+        if (answer1 === answers.q1){
+            score()
+            console.log("Correct: " + correct)  
+        }
+});
+
+$('.card-body2 input').on('change', function() {
+    answer2 = $('input[name=inlineRadioOptions2]:checked', '.card-body2').val();
+        if (answer2 === answers.q2){
+            score()
+            console.log("Correct: " + correct)            
+        }  
+});
+
+$('.card-body3 input').on('change', function() {
+    answer3 = $('input[name=inlineRadioOptions3]:checked', '.card-body3').val();
+        if (answer3 === answers.q3){
+            score()
+            
+            console.log("Correct: " + correct)
+        }  
+});
+
+$('.card-body4 input').on('change', function() {
+    answer4 = $('input[name=inlineRadioOptions4]:checked', '.card-body4').val();
+        if (answer4 === answers.q4){
+            score()
+            console.log("Correct: " + correct)
+        }  
+});
+
+$('.card-body5 input').on('change', function() {
+    answer5 = $('input[name=inlineRadioOptions5]:checked', '.card-body5').val();
+        if (answer5 === answers.q5){
+            score()
+            console.log("Correct: " + correct)
+        }  
+});
+
+$('.card-body6 input').on('change', function() {
+    answer6 = $('input[name=inlineRadioOptions6]:checked', '.card-body6').val();
+        if (answer6 === answers.q6){
+            score()
+            console.log("Correct: " + correct)
+        }  
+});
+
+$('.card-body7 input').on('change', function() {
+    answer7 = $('input[name=inlineRadioOptions7]:checked', '.card-body7').val();
+        if (answer7 === answers.q7){
+            score()
+            console.log("Correct: " + correct)
+        }  
+});
+
+$('.card-body8 input').on('change', function() {
+    answer8 = $('input[name=inlineRadioOptions8]:checked', '.card-body8').val();
+        if (answer8 === answers.q8){
+            score()
+            console.log("Correct: " + correct)
+        }  
+});
+
+$('.card-body9 input').on('change', function() {
+    answer9 = $('input[name=inlineRadioOptions9]:checked', '.card-body9').val();
+        if (answer9 === answers.q9){
+            score()
+            console.log("Correct: " + correct)
+        }  
+});
+
+$('.card-body10 input').on('change', function() {
+    answer10 = $('input[name=inlineRadioOptions10]:checked', '.card-body10').val();
+        if (answer10 === answers.q10){
+            score()
+            console.log("Correct: " + correct)            
+        }  
 });
 
 
 
+function score(){
+    correct++
+    $("#correct").empty().append(correct + " out of 10")
+    wrong = 10 - correct
+    $("#wrong").empty().append(wrong + " out of 10");
+    let score = (correct/10)*100
+    $("#score").empty().append(score + "%");
+};
+
+    //  }
+     
+   
+
+
+
+
+
+   
+    
+   
+
+
+
+
+
+
+ });
+
+ 
+
+
+//  var selected = $('input[name=inlineRadioOptions0]:checked', '.form-check').val()
+// console.log(selected)
+// $('.form-check input').on('change', function() {
+// 	console.log(selected)
+// });
+
+// $('input[name=radioName]:checked', '#myForm').val()
+// $('#myForm input').on('change', function() {
+// 	$('#val').text($('input[name=radioName]:checked', '#myForm').val());
+// });
+
+
+// console.log(answers.q1)
+
+
+
+//  if (selected === answers.q1){
+//      correct++
+//      console.log("Correct: " + correct)
+//  }
+//  else {
+//      wrong++
+//      console.log("Wrong: " + wrong)
+//  }
 
 
 
@@ -74,6 +228,8 @@ $("#submitBtn").on("click", function () {
 
 
 
+
+// ("Correct: " + correct), ("Wrong: " + wrong))
 
 
 
